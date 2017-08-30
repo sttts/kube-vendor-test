c3136d01384dba3efd576e8a504c9a50ee97e3e6
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2017-08-30 12:15:27 +0200

    Import all the packages so that constraints apply
    Compare https://github.com/golang/dep/issues/1090

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
-	_ "k8s.io/kube-aggregator/pkg/apiserver"
+	_ "k8s.io/kube-aggregator"
+	_ "k8s.io/apiserver/pkg/storage"
+	_ "k8s.io/client-go/kubernetes"
+	_ "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func main() {
